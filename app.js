2721c9058c2c57fd3689a4dac476ed913b7cc9e2
Michael <mikemcd.bbs@gmail.com>
2023-09-12 22:13:26 -0400

    Add preliminary js

---
+++ app.js
@@ -0,0 +1,7 @@
+const calculatorResult = document.querySelector(".calculator-result");
+const calculatorSymbols = document.querySelectorAll(".operation");
+const calculatorNumbers = document.querySelectorAll(".number");
+
+console.log(calculatorResult);
+console.table(calculatorOperations);
+console.table(calculatorNumbers);
